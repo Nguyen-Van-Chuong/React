3923f58c9d7c1fa6ee58fcc5df070582d9565575
Chuong Nguyen <chuongsanji@gmail.com>
2023-07-02 14:18:36 +0700

    setup vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import RegisterHook from "./components/form/RegisterHook";
+
+function App() {
+  return (
+    <>
+      <RegisterHook></RegisterHook>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/form/RegisterHook.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+const RegisterHook = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus dolores
+      ad distinctio esse culpa possimus tempore quam aspernatur perspiciatis,
+      minus ea error nemo, labore cum saepe minima molestiae beatae quia.
+    </div>
+  );
+};
+
+export default RegisterHook;
