2d57961662c6b3ac439e5e9549688954b9993d6b
Chuong Nguyen <chuongsanji@gmail.com>
2023-07-29 14:39:34 +0700

    update add delete todo

---
+++ src/context/index.jsx
@@ -0,0 +1,34 @@
+import React, { createContext } from "react";
+import { useState } from "react";
+import {
+  useFilterTodos,
+  useProjects,
+  useProjectsWithStats,
+  useTodos,
+} from "../hooks";
+const TodoContext = createContext();
+
+function TodoContextProvider({ children }) {
+  const defaultProject = "today";
+  const [selectedProject, setSelectedProject] = useState(defaultProject);
+
+  const todos = useTodos();
+  const projects = useProjects();
+  const projectsWithStats = useProjectsWithStats(projects, todos);
+  const filteredTodos = useFilterTodos(todos, selectedProject);
+  return (
+    <TodoContext.Provider
+      value={{
+        defaultProject,
+        selectedProject,
+        setSelectedProject,
+        todos: filteredTodos,
+        projects: projectsWithStats,
+      }}
+    >
+      {children}
+    </TodoContext.Provider>
+  );
+}
+
+export { TodoContextProvider, TodoContext };
